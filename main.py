a49565732f70ba6dd9f363725f2f440cbf078c77
Steve108 <liuzhiyu.sh@outlook.com>
2024-08-04 14:13:26 +0800

    Add GPL2 Header
    and header template

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+# ModLibrary, a Minecraft mod manager
+# Copyright (C) 2024  iamliuzy
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Contact the author: iamliuzy <liuzhiyu.sh@outlook.com>
+
 """Main Entry."""
-from pathlib import Path
 import sys
 import os
-from PyQt6 import QtWidgets, QtGui, QtCore
+from PyQt6 import QtWidgets, QtGui
 import constants as const
 from constants import tr
 import jsonp
-import jsons
-import mods
 
 import qfluentwidgets as qfw
 
